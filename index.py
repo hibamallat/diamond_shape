2094fb3a8229d6b078ec9f1b5c4b8b8975d1ad9e
hibamallat <hibamallat040@gmail.com>
2024-10-31 13:05:03 +0200

    file updated

--- index.py
+++ index.py
@@ -1,22 +1,22 @@
-r = int(input("enter number of rows: "))
+row_num = int(input("enter number of rows: "))
 #The top half of the diamond
-for i in range(1, r + 1):
+for i in range(1, row_num + 1):
 
-    for j in range(r - i + 1):
+    for j in range(row_num - i + 1):
         print(" ", end = '') 
 
-    for k in range (2 * i - 1):
+    for j in range (2 * i - 1):
         print("*", end = '')
 
     print() # Move to the next line after completing the row
 
 # The bottom half of the diamond
-for i in range (r - 1, 0, -1):
+for i in range (row_num - 1, 0, -1):
 
-    for j in range (r - i + 1):
+    for j in range (row_num - i + 1):
         print(" ", end = '')
 
-    for k in range (2 * i - 1):
+    for j in range (2 * i - 1):
         print("*", end = '')
 
     print() 
