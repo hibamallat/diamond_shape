d0b2909395a8c170a4392c5c7fa334356032db2f
hibamallat <hibamallat040@gmail.com>
2024-11-02 02:11:13 +0200

    input validation

--- index.py
+++ index.py
@@ -1,29 +1,33 @@
-row_num = int(input("enter number of rows: "))
+try:
+    row_num = int(input("enter number of rows: "))
 
-if row_num <= 0:
-    print("The number should be positive")
-    
-#The top half of the diamond
-for i in range(1, row_num + 1):
+    if row_num <= 0:
+        print("INVALID:The number should be positive")
 
-    for j in range(row_num - i + 1):
-        print(" ", end = '') 
+    #The top half of the diamond
+    for i in range(1, row_num + 1):
 
-    for j in range (2 * i - 1):
-        print("*", end = '')
+        for j in range(row_num - i + 1):
+            print(" ", end = '') 
 
-    print() # Move to the next line after completing the row
+        for j in range (2 * i - 1):
+            print("*", end = '')
 
-# The bottom half of the diamond
-for i in range (row_num - 1, 0, -1):
+        print() # Move to the next line after completing the row
 
-    for j in range (row_num - i + 1):
-        print(" ", end = '')
+    #The bottom half of the diamond
+    for i in range (row_num - 1, 0, -1):
 
-    for j in range (2 * i - 1):
-        print("*", end = '')
+        for j in range (row_num - i + 1):
+            print(" ", end = '')
 
-    print() 
+        for j in range (2 * i - 1):
+            print("*", end = '')
+
+        print() 
+        
+except ValueError:
+    print("INVALID:Enter only a number")
 
     
     
