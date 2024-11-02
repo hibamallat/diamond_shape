64535a7605893c1912d09dbe4db1558412e2b4fe
hibamallat <hibamallat040@gmail.com>
2024-11-02 02:45:54 +0200

    Add some additional comment lines

--- index.py
+++ index.py
@@ -1,16 +1,18 @@
 try:
+    #enter the number of rows for the diamond shape
     row_num = int(input("enter number of rows: "))
 
+    # Check if the entered number is positive
     if row_num <= 0:
         print("INVALID:The number should be positive")
 
     #The top half of the diamond
     for i in range(1, row_num + 1):
 
-        for j in range(row_num - i + 1):
-            print(" ", end = '') 
+        for j in range(row_num - i + 1): #Print spaces before the stars to center the diamond
+            print(" ", end = '')
 
-        for j in range (2 * i - 1):
+        for j in range (2 * i - 1): # Print the stars for the current row
             print("*", end = '')
 
         print() # Move to the next line after completing the row
@@ -24,11 +26,8 @@ try:
         for j in range (2 * i - 1):
             print("*", end = '')
 
-        print() 
-        
-except ValueError:
-    print("INVALID:Enter only a number")
+        print()
 
-    
-    
-         
+except ValueError:
+    # Handle the case where the input is not a valid integer
+    print("INVALID:Enter only a number")
